aff3072bcb3f85190ab04ec24e915eae5b36f189
Game2njoy <sg851033@gmail.com>
2024-04-13 02:32:57 +0900

    사이트맵

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mom.urls')),
     path('robots.txt/', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+    path('sitemap.xml/', TemplateView.as_view(template_name="sitemap.xml", content_type='application/xml')),
 ]
 
 handler404 = 'mom.views.page_not_found'
