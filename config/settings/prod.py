46e4029afc969d7f433a0a1004575a3fc47b5fc0
Game2njoy <sg851033@gmail.com>
2024-04-12 21:01:40 +0900

    도메인 설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['15.164.186.140', 'xn--oy2b15bl6nb1dcwo.com']
+ALLOWED_HOSTS = ['15.164.186.140', 'xn--oy2b15bl6nb1dcwo.com', 'www.xn--oy2b15bl6nb1dcwo.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
